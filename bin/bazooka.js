968e42185d4b6bdd0a4be951c5fe0eaa9b3c0741
Clyde Smets <clyde.smets@gmail.com>
2017-02-13 20:53:03 +1000

    Added file bazooka-compile

--- bin/bazooka.js
+++ bin/bazooka.js
@@ -2,8 +2,7 @@
 
 const fs = require('fs');
 const Client = require('ssh2').Client;
-const path = require('path');
-const mdtojson = require('../lib/mdtojson');
+const bazooka = require('../lib/bazooka-compile');
 
 // Path of where command is executed from
 const workingDir = process.cwd();
@@ -17,52 +16,11 @@ const config = JSON.parse(configFile);
  * EXPORT
  * ==========================================================================*/
 
-const getDirectories = (srcpath) =>
-	fs.readdirSync(srcpath)
-		.filter(file => fs.statSync(path.join(srcpath, file)).isDirectory());
+const json = bazooka.compile(process.cwd(), config.excludeDirs, {});
+const jsonString = JSON.stringify(json, null, 4);
 
-const getFiles = (srcpath) =>
-	fs.readdirSync(srcpath)
-		.filter(file =>
-			fs.statSync(path.join(srcpath, file)).isDirectory() === false);
-
-const crawlnstore = (crawlPath, obj) => {
-	const dirs = getDirectories(crawlPath);
-	const excludeDirs = dirs.filter(dir => config.excludeDirs.indexOf(dir) < 0);
-
-	const newObj = {};
-	excludeDirs.forEach((dir) => {
-		const files = getFiles(crawlPath);
-		const folderContents = {};
-		files.forEach((file) => {
-			const filejson = mdtojson.process(`${crawlPath}/${file}`);
-			const fileExtention = path.extname(file);
-			const filename = file.replace(fileExtention, '');
-			folderContents[filename] = filejson;
-		});
-		newObj.pages = folderContents;
-		newObj[dir] = crawlnstore(`${crawlPath}/${dir}`, {});
-	});
-
-	if (dirs.length > 0) {
-		return Object.assign(obj, newObj);
-	}
-
-	return {};
-};
-
-const compile = (outputFile) => {
-	const json = crawlnstore(process.cwd(), {});
-	const jsonString = JSON.stringify(json, null, 4);
-
-	// Write to file
-	fs.writeFileSync(outputFile, jsonString);
-
-	return json;
-};
-
-// Compile and create temp file
-compile('export.json');
+// Write to file
+fs.writeFileSync('export.json', jsonString);
 
 /* ============================================================================
  * REMOTE UPLOAD
